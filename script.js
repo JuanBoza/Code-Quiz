f50c899fa07cdd1d7752511084db1139620001c9
Juan Boza <Juanboza80@gmail.com>
2021-02-07 00:00:10 -0700

    index.html, style.css, and script.js files created

---
+++ script.js
@@ -0,0 +1,29 @@
+var questions = [
+    {
+        prompt: 
+        answer: 
+
+    }, 
+    {
+        prompt: 
+        answer: 
+
+
+    }
+
+
+
+
+]
+
+for(var i=0; i <questions.lenth; i++){
+    var response = window.prompt(questions[i].prompt); 
+    if(response == questions[i].answsers){
+        score++; 
+        alert("Correct!"); 
+    } else { 
+        alert("Wrong!"); 
+    }
+}
+
+alert("you got" + score + "/" + questions.length); 
