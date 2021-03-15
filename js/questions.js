ca4a5ca90b75efffce34b9b54b15af066196fe59
Juan Boza <Juanboza80@gmail.com>
2021-03-15 15:44:25 -0600

    working and finished

---
+++ js/questions.js
@@ -0,0 +1,40 @@
+
+let questions = [
+    {
+    numb: 1,
+    question: "Inside which HTML element do we put the JavaScript?",
+    answer: "script",
+    options: ["script","js","scripting","javascript"]
+
+  },
+    {
+    numb: 2,
+    question: "Which of the following function of Boolean object returns a string containing the source of the Boolean object?",
+    answer: "toSource()",
+    options: ["toSource()","valueOf()","toString()","None of the Above"]
+
+  },
+    {
+    numb: 3,
+    question: "Which tool can you use to ensure code quality?",
+    answer: "ESLint",
+    options: ["Angular","jQuery","RequireJS","ESLint"],
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which one of these is a JavaScript package manager??",
+    answer: "npm",
+    options: ["Node.js","TypScript","npm","MySQL"],
+  },
+];
